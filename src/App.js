24625fdbf6a7c3089ca3f6bde882683e55ce205c
MattMorrell15 <morrel18@msu.edu>
2024-04-06 04:47:47 -0400

    addition of functions and constants

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+const magazines = [
+  { id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true },
+  { id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true },
+  { id: 3, title: 'Communication Arts', theme: 'design', isAvailable: false },
+];
+
+function ZineRack() {
+  const listZines = magazines.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'red' : 'green'
+      }}
+    >
+      {zine.title}
+    </li>
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+
+const book = {
+  title: 'A Farewell to Arms',
+  author: 'Earnest Hemingway',
+  published: '1929',
+  image: logo,
+  width: '264',
+  height: '378'
+};
+
+function Bookshelf() {
+  return (
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      <img
+        className="bookCover"
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+      />
+    </div>
+  );
+}
+
 function Magic() {
   return (
-    <button>MagicButton</button>
+    <div>
+      <h3>This is a magic button</h3>
+      <button>MagicButton</button>
+    </div>
   );
 }
 
@@ -11,20 +63,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Matt and MSU
-        </a>
+        <ZineRack />
+        <Bookshelf />
       </header>
-      <Magic />
     </div>
   );
 }
